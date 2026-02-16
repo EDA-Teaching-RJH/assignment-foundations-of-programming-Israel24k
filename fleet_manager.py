bffc4c5612e05f3fef2450d7efb230ef769c7d3f
Israel24k <ii13@kent.ac.uk>
2026-02-16 01:48:39 +0000

    1

---
+++ fleet_manager.py
@@ -0,0 +1,9 @@
+def init_database():
+    names = ["Captain_James","Ricker","Dr_Leonard","Worf","Data"]
+    ranks = ["Captain","Commander","Admiral","Lieutenant","Lt_Commander"]
+    ids = [1,2,3,4,5]
+    divs = ["Engineering", "Command","Medical","Security","Operations"]
+
+    return names,ranks,ids,divs
+
+def d
